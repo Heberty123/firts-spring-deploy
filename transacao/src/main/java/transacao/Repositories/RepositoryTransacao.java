8bb323f34df61ee3fb326bc6e163cdbd642af5ef
Heberty123 <hebertyprado22005@gmail.com>
2022-05-09 15:15:37 -0300

    att

--- transacao/src/main/java/transacao/Repositories/RepositoryTransacao.java
+++ transacao/src/main/java/transacao/Repositories/RepositoryTransacao.java
@@ -18,16 +18,16 @@ public interface RepositoryTransacao extends JpaRepository<Transacao, Long> {
 	@Query("SELECT t FROM Transacao t WHERE t.valor >= :value AND MONTH(t.data) = :month AND YEAR(t.data) = :year")
 	List<Transacao> findAllSusTransactions(Double value, int month, int year);
 
-	@Query("SELECT t.bancoOrigem, t.agenciaOrigem, t.contaOrigem, SUM(t.valor) FROM Transacao t WHERE MONTH(t.data) = :month AND YEAR(t.data) = :year GROUP BY t.contaOrigem")
+	@Query("SELECT t.bancoOrigem, t.agenciaOrigem, t.contaOrigem, SUM(t.valor) FROM Transacao t WHERE MONTH(t.data) = :month AND YEAR(t.data) = :year GROUP BY t.bancoOrigem, t.agenciaOrigem, t.contaOrigem")
 	List<String> findOrigemAccWithMonthAndYear(int month, int year);
 	
-	@Query("SELECT t.bancoDestino, t.agenciaDestino, t.contaDestino, SUM(t.valor) FROM Transacao t WHERE MONTH(t.data) = :month AND YEAR(t.data) = :year GROUP BY t.contaDestino")
+	@Query("SELECT t.bancoDestino, t.agenciaDestino, t.contaDestino, SUM(t.valor) FROM Transacao t WHERE MONTH(t.data) = :month AND YEAR(t.data) = :year GROUP BY t.bancoDestino, t.agenciaDestino, t.contaDestino")
 	List<String> findDestinoAccWithMonthAndYear(int month, int year);
 	
-	@Query("SELECT t.bancoOrigem, t.agenciaOrigem, SUM(t.valor) FROM Transacao t WHERE MONTH(t.data) = :month AND YEAR(t.data) = :year GROUP BY t.agenciaOrigem")
+	@Query("SELECT t.bancoOrigem, t.agenciaOrigem, SUM(t.valor) FROM Transacao t WHERE MONTH(t.data) = :month AND YEAR(t.data) = :year GROUP BY t.bancoOrigem, t.agenciaOrigem")
 	List<String> findAllOrigemAgencyWithMonthAndYear(int month, int year);
 	
-	@Query("SELECT t.bancoDestino, t.agenciaDestino, SUM(t.valor) FROM Transacao t WHERE MONTH(t.data) = :month AND YEAR(t.data) = :year GROUP BY t.agenciaDestino")
+	@Query("SELECT t.bancoDestino, t.agenciaDestino, SUM(t.valor) FROM Transacao t WHERE MONTH(t.data) = :month AND YEAR(t.data) = :year GROUP BY t.bancoDestino, t.agenciaDestino")
 	List<String> findAllDestinoAgencyWithMonthAndYear(int month, int year);
 	
 	
